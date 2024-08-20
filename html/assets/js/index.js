3659e88ef16d0a65dba6ed6bc8f44200f90f7e94
Luan <luuans27@hotmail.com>
2024-08-20 20:25:48 -0300

    atulização do codigo javascript, o mesmo estava apresentando erros de execução

--- html/assets/js/index.js
+++ html/assets/js/index.js
@@ -29,14 +29,14 @@ form.addEventListener('submit', function (e) {
 
 function getNivelImc (imc) {
     const nivel =  ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
-         'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
 
-    if (imc >= 39.9) return nivel [5];
-    if (imc >= 34.9) return nivel [4];
-    if (imc >= 29.9) return nivel [3];
-    if (imc >= 18.5) return nivel [2];
-    if (imc >= 18.5) return nivel [1];
-    if (imc >= 18.5) return nivel [0];
+        if (imc >= 39.9) return nivel[5];
+        if (imc >= 34.9) return nivel[4];
+        if (imc >= 29.9) return nivel[3];
+        if (imc >= 24.9) return nivel[2]; // Peso normal
+        if (imc >= 18.5) return nivel[1]; // Abaixo do peso
+        return nivel[0]; // Caso o IMC seja menor que 18.5
     
 }
 
@@ -63,5 +63,5 @@ function setResultado (msg, isValid) {
     }
 
     p.innerHTML = msg;
-    resultado.appendChild('p');
+    resultado.appendChild(p);
 }
